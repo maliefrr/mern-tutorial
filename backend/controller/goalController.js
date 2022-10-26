d2a95923058d75c4e30b311c291414a978651dcd
maliefrr <aliefm144@gmail.com>
2022-10-26 12:55:21 +0800

    initial rest api for goals

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,12 +1,15 @@
 const asyncHandler = require('express-async-handler')
+const goalModel = require('../models/goalModel')
 
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
 const getGoals = asyncHandler(async (req,res) => {
+    const goals = await goalModel.find()
+
         res.status(200).json({
             statusCode: 200,
-            message: "Get Goals"
+            data: goals
     })
 } )
 
@@ -14,13 +17,18 @@ const getGoals = asyncHandler(async (req,res) => {
 // @route POST /api/goals
 // @access Private
 const setGoal = asyncHandler(async (req,res) => {
-    if(!req.body.name){
+    if(!req.body.text){
         res.status(400)
-        throw new Error("Please fill the name field")
+        throw new Error("Please fill the text field")
     }
+    const goal = await goalModel.create({
+        text: req.body.text
+    })
+
     res.status(200).json({
         statusCode : 200,
-        message: "Create Goal"
+        message: "The data has been successfully created",
+        data: goal
     })
 })
 
@@ -28,9 +36,22 @@ const setGoal = asyncHandler(async (req,res) => {
 // @route PUT /api/goals/:id
 // @access Private
 const updateGoal = asyncHandler(async (req,res) => {
+    const goal = goalModel.findById(req.params.id)
+
+    if(!goal){
+        res.status(404)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await goalModel.findByIdAndUpdate(req.params.id,req.body,{
+        new: true,
+    })
+
+
     res.status(200).json({
         statusCode : 200,
-        message: `Update Goal ${req.params.id}`
+        message: `Goal with id ${req.params.id} has been successfully updated`,
+        data: updatedGoal
     })
 })
 
@@ -38,9 +59,19 @@ const updateGoal = asyncHandler(async (req,res) => {
 // @route DELETE /api/goals/:id
 // @access Private
 const deleteGoal = asyncHandler(async (req,res) => {
+    const goal = await goalModel.findById(req.params.id);
+
+    if(!goal){
+        res.status(404)
+        throw new Error("Goal not found")
+    }
+
+    const deletedGoal = await goalModel.findByIdAndDelete(req.params.id)
+
     res.status(200).json({
         statusCode : 200,
-        message: `Delete goal ${req.params.id}`
+        message: `Goal with id ${req.params.id} successfully deleted`,
+        data: deletedGoal
     })
 })
 
